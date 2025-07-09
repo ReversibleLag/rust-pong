8fd024c810e17a1384036585f96906308eda05ab
Kai South <kaisouth123@gmail.com>
2025-07-09 01:15:54 -0400

    added scene reset and score counter, need to display it

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use std::i32;
 use std::time::Duration;
 use sdl2::rect::Rect;
 use rand::prelude::*;
+// use sdl2::ttf::Sdl2TtfContext;
 
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -27,7 +28,10 @@ pub fn main() {
     let mut diagonal = 0;
     let mut left_rect = Rect::new(50, 350, 40, 80);
     let mut ball = Rect::new(viewport.w/2 - 10, viewport.h/2 - 10, 20, 20);
-    let right_rect = Rect::new(710, 50, 40, 80);
+    let mut right_rect = Rect::new(710, 50, 40, 80);
+
+    let mut player_score =0;
+    let mut enemy_score =0;
 
     let mut towards_player = true;
 
@@ -98,10 +102,31 @@ pub fn main() {
         }
         if is_touching_win(&ball, 800, 600){
             println!("Win");
+            //reset scene
+            // add point for left side
+            ball.x = viewport.w/2 - 10;
+            ball.y = viewport.h/2 - 10;
+            left_rect.y = viewport.h/2 - 80;
 
+            right_rect.y = viewport.h/2 - 80;
+
+            diagonal = rng.random_range(-5..5);
+            towards_player = rng.random_bool(1.0/3.0);
+            player_score += 1;
         }else if is_touching_loss(&ball, 800, 600)
         {
             println!("Lose");
+            //reset scene
+            // add point for right side
+            ball.x = viewport.w/2 - 10;
+            ball.y = viewport.h/2 - 10;
+            left_rect.y = viewport.h/2 - 80;
+
+            right_rect.y = viewport.h/2 - 80;
+
+            diagonal = rng.random_range(-5..5);
+            enemy_score += 1;
+            towards_player = rng.random_bool(1.0/3.0);
         }
 
 
